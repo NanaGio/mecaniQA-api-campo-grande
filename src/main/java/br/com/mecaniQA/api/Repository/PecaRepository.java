74b65602a9066502b9c83345a77f64374f04a369
NanaGio <giovanna000gio@outlook.com>
2026-08-18 12:21:49 -0300

    repository/ parte 2

--- src/main/java/br/com/mecaniQA/api/Repository/PecaRepository.java
+++ src/main/java/br/com/mecaniQA/api/Repository/PecaRepository.java
@@ -1,6 +1,24 @@
 package br.com.mecaniQA.api.Repository;
 
+//Fontes de base: medium.com/@adityashete009/implementing-singleton-classes-in-java | devmedia.com.br/padrao-de-projeto-singleton-em-java | Singleton em Java (Lucas Herom) / Youtube
+
 //Acessar sempre via getInstance(), nunca new, para não quebrar a regra do singleton.
 
 public class PecaRepository {
+    // Criando instância
+    private static PecaRepository INSTANCE;
+
+    //O construtor é privado evitando que essa classe seja instanciada fora dela.
+    private PecaRepository(){
+
+    }
+
+    //Metodo publico para instância. Aqui escolhi incluir o synchronized para evitar a criação de múltiplos, sincronizando com a instância atual.
+    public static synchronized PecaRepository getInstance(){
+        if(INSTANCE == null){
+            INSTANCE = new PecaRepository();
+        }
+        return INSTANCE;
+    }
+    //Outros métodos, get ou setters...
 }
--- src/main/java/br/com/mecaniQA/api/Repository/ServicoRepository.java
+++ src/main/java/br/com/mecaniQA/api/Repository/ServicoRepository.java
@@ -1,6 +1,24 @@
 package br.com.mecaniQA.api.Repository;
 
+//Fontes de base: medium.com/@adityashete009/implementing-singleton-classes-in-java | devmedia.com.br/padrao-de-projeto-singleton-em-java | Singleton em Java (Lucas Herom) / Youtube
+
 //Acessar sempre via getInstance(), nunca new, para não quebrar a regra do singleton.
 
 public class ServicoRepository {
+    // Criando instância
+    private static ServicoRepository INSTANCE;
+
+    //O construtor é privado evitando que essa classe seja instanciada fora dela.
+    private ServicoRepository(){
+
+    }
+
+    //Metodo publico para instância. Aqui escolhi incluir o synchronized para evitar a criação de múltiplos, sincronizando com a instância atual.
+    public static synchronized ServicoRepository getInstance(){
+        if(INSTANCE == null){
+            INSTANCE = new ServicoRepository();
+        }
+        return INSTANCE;
+    }
+    //Outros métodos, get ou setters...
 }
