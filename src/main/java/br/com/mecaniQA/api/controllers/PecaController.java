98e8dff738c15210d380625aeaa012653e511f1b
João Mira <jmirauniversitario@gmail.com>
2026-08-18 22:57:27 -0300

    feat: adicionando diretórios de controllers

---
+++ src/main/java/br/com/mecaniQA/api/controllers/PecaController.java
@@ -0,0 +1,6 @@
+package br.com.mecaniQA.api.controllers;
+
+public class PecaController {
+    // A estrutura de rotas deve seguir o padrão RESTful (ex: /api/pecas e /api/servicos)
+    // Utilizar os verbos HTTP corretos para cada ação mapeando-os com as anotações do Spring
+}
---
+++ src/main/java/br/com/mecaniQA/api/controllers/ServicoController.java
@@ -0,0 +1,6 @@
+package br.com.mecaniQA.api.controllers;
+
+public class ServicoController {
+    // A estrutura de rotas deve seguir o padrão RESTful (ex: /api/pecas e /api/servicos)
+    // Utilizar os verbos HTTP corretos para cada ação mapeando-os com as anotações do Spring
+}
