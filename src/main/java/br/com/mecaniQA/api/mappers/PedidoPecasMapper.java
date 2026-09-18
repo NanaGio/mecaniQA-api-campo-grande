8252f31e9052da57f8761a4229c39f26d55433b3
NanaGio <giovanna000gio@outlook.com>
2026-09-18 16:00:33 -0300

    continuando mappers - pedido peças

--- src/main/java/br/com/mecaniQA/api/mappers/PedidoPecasMapper.java
+++ src/main/java/br/com/mecaniQA/api/mappers/PedidoPecasMapper.java
@@ -1,6 +1,28 @@
 package br.com.mecaniQA.api.mappers;
 
 //Precisa de lista aqui
+//CONTINUAR
+
+import br.com.mecaniQA.api.DTO.ItemPedidoDTO;
+import br.com.mecaniQA.api.DTO.PedidoPecasDTO;
+import br.com.mecaniQA.api.model.ItemPedido;
+import br.com.mecaniQA.api.model.PedidoPecas;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PedidoPecasMapper {
+    public static PedidoPecas toEntity(PedidoPecasDTO dto){
+        PedidoPecas pedido = new PedidoPecas();
+        pedido.setId(dto.getId());
+        pedido.setPeca(dto.getPeca());
+        pedido.setStatus(dto.getStatus());
+
+        List<ItemPedidoDTO> itens = dto.getItens().stream()
+                .map(ItemPedidoMapper::toEntity)
+                .collect(Collectors.toList());
+        pedido.setItens(itens);
+
+        return pedido;
+    }
 }
